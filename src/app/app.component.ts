8f8d1d5ec8b852ee617bd23efc9b29a10b736982
necm1 <necmi.m.hasan@outlook.de>
2022-02-13 18:21:03 +0100

    [UPDATE] app.component.ts - added basic commenting & refactored selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'insel-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'insel-frontend';
-}
+/**
+ * @class AppComponent
+ */
+export class AppComponent {}
