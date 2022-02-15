a95e9041facc9a63470e297d8f2e594fa3374dbe
necm1 <necmi.m.hasan@outlook.de>
2022-02-15 21:42:32 +0100

    [UPDATE] map.component.ts - refactored selector

--- src/app/hud/component/map/map.component.ts
+++ src/app/hud/component/map/map.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'insel-map',
+  selector: 'insel-hud-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
+/**
+ * @class MapComponent
+ * @implements {OnInit}
+ */
 export class MapComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
