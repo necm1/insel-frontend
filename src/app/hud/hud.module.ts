826096884a22818b7ee14ded8d7b6147d7f5d5fa
necm1 <necmi.m.hasan@outlook.de>
2022-02-15 19:48:48 +0100

    [UPDATE] hud.module.ts - added LogoComponent

--- src/app/hud/hud.module.ts
+++ src/app/hud/hud.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {LogoComponent} from './component/logo/logo.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LogoComponent],
   imports: [CommonModule],
+  exports: [LogoComponent],
 })
 /**
  * @class HUDModule
