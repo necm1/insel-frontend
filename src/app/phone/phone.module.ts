c8ece063b8e5b83fe0995336f11617550ba94330
necm1 <necmi.m.hasan@outlook.de>
2022-02-15 18:21:13 +0100

    [UPDATE] phone.module.ts - added PhoneComponet, HomeComponent & PhoneService

--- src/app/phone/phone.module.ts
+++ src/app/phone/phone.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PhoneComponent} from './component/phone.component';
+import {HomeComponent} from './component/home/home.component';
+import {PhoneService} from './service/phone.service';
 
 @NgModule({
-  declarations: [PhoneComponent],
+  declarations: [PhoneComponent, HomeComponent],
   imports: [CommonModule],
   exports: [PhoneComponent],
+  providers: [PhoneService],
 })
 /**
  * @class PhoneModule
