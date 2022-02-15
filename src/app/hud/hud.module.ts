40d1c96fae33ac65c8a9b3b0f9b23fec547bd4ac
necm1 <necmi.m.hasan@outlook.de>
2022-02-15 22:14:35 +0100

    [UPDATE] hud.module.ts - added StatsComponent

--- src/app/hud/hud.module.ts
+++ src/app/hud/hud.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from '@angular/common';
 import {LogoComponent} from './component/logo/logo.component';
 import {MapComponent} from './component/map/map.component';
 import {HUDComponent} from './component/hud.component';
+import {StatsComponent} from './component/stats/stats.component';
 
 @NgModule({
-  declarations: [LogoComponent, MapComponent, HUDComponent],
+  declarations: [LogoComponent, MapComponent, HUDComponent, StatsComponent],
   imports: [CommonModule],
   exports: [HUDComponent],
 })
