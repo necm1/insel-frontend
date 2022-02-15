a97fde238ce1e8eadf37caa8f823287c9deaa6d9
necm1 <necmi.m.hasan@outlook.de>
2022-02-15 21:41:27 +0100

    [CREATE] hud.component.*

---
+++ src/app/hud/component/hud.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'insel-hud',
+  templateUrl: './hud.component.html',
+  styleUrls: ['./hud.component.scss'],
+})
+export class HUDComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
