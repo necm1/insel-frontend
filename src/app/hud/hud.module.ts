4534b406b7eae5b66bd57602b70a378905f8b376
necm1 <necmi.m.hasan@outlook.de>
2022-02-15 21:40:31 +0100

    [UPDATE] hud.module.ts - added HudComponent

--- src/app/hud/hud.module.ts
+++ src/app/hud/hud.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LogoComponent} from './component/logo/logo.component';
+import {MapComponent} from './component/map/map.component';
+import {HudComponent} from './component/hud.component';
 
 @NgModule({
-  declarations: [LogoComponent],
+  declarations: [LogoComponent, MapComponent, HudComponent],
   imports: [CommonModule],
-  exports: [LogoComponent],
+  exports: [HudComponent],
 })
 /**
  * @class HUDModule
