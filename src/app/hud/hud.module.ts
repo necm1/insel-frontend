71ba13044ffc426692270dc455bbf570f0d4cb88
necm1 <necmi.m.hasan@outlook.de>
2022-03-10 19:01:48 +0100

    [UPDATE] hud.module.ts - added ProgressbarModule

--- src/app/hud/hud.module.ts
+++ src/app/hud/hud.module.ts
@@ -4,10 +4,11 @@ import {LogoComponent} from './component/logo/logo.component';
 import {MapComponent} from './component/map/map.component';
 import {HUDComponent} from './component/hud.component';
 import {StatsComponent} from './component/stats/stats.component';
+import {ProgressbarModule} from 'ngx-bootstrap/progressbar';
 
 @NgModule({
   declarations: [LogoComponent, MapComponent, HUDComponent, StatsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ProgressbarModule],
   exports: [HUDComponent],
 })
 /**
