5fac782c3b4609d6df762021e203b231f3a3cff3
necm1 <necmi.m.hasan@outlook.de>
2022-03-10 19:01:07 +0100

    [UPDATE] app.module.ts - added ChildShopModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {PhoneModule} from './phone/phone.module';
 import {HUDModule} from './hud/hud.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ProgressbarModule} from 'ngx-bootstrap/progressbar';
+import {ChildShopModule} from './child-shop/child-shop.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NgbModule, PhoneModule, HUDModule],
+  imports: [
+    BrowserModule,
+    PhoneModule,
+    HUDModule,
+    BrowserAnimationsModule,
+    ProgressbarModule.forRoot(),
+    ChildShopModule,
+  ],
   bootstrap: [AppComponent],
 })
 /**
