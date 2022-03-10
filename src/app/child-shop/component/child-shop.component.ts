ef161cab2a28c0931f191053098e6e0dfa3b16aa
necm1 <necmi.m.hasan@outlook.de>
2022-03-10 19:04:32 +0100

    [CREATE] child-shop.component.*

---
+++ src/app/child-shop/component/child-shop.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'insel-child-shop',
+  templateUrl: './child-shop.component.html',
+  styleUrls: ['./child-shop.component.scss'],
+})
+/**
+ * @class ChildShopComponent
+ * @implements {OnInit}
+ */
+export class ChildShopComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
