c7483cfc755f9468c30be2f4090f4a8d25a53d5f
necm1 <necmi.m.hasan@outlook.de>
2022-03-10 19:04:16 +0100

    [UPDATE] child-shop.module.ts - added ChildShopComponent

--- src/app/child-shop/child-shop.module.ts
+++ src/app/child-shop/child-shop.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ChildShopComponent} from './component/child-shop.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ChildShopComponent],
   imports: [CommonModule],
 })
 /**
